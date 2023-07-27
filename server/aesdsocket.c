18f4ef0634d48e264707778deab293af4a42e05a
root <root@Ubuntu.myguest.virtualbox.org>
2023-07-27 01:15:00 -0300

    Assignment 5 part 1 - reading and sending to socket

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -9,6 +9,11 @@
 #include <netdb.h>
 #include <arpa/inet.h>
 
+#define BUFFER_SIZE 2
+
+int read_socket(int);
+int send_socket(int);
+
 int main(int argc, char *argv[]){
 
 	int sockfd = socket(PF_INET, SOCK_STREAM, 0); //returns a file descriptor
@@ -64,23 +69,85 @@ int main(int argc, char *argv[]){
 
 	syslog(LOG_USER,"Accepted connection from %s",client_ip);
 	printf("Accepted connection from %s\n",client_ip);
-
-	/*int stop_received=0;
-	while(stop_received==0){
-
-        	file = open("/var/tmp/aesdsocketdata", O_RDWR | O_CREAT | O_TRUNC, 0755);
-
-        	if (write(file, text, strlen(text))==-1){
-               		close(file);
-                	return 1;
-        	}
-
-        	close(file);
-	}//*/
-
 	
+	if(read_socket(acceptedfd)==-1){
+		return -1;
+	}
+
+
+	if(send_socket(acceptedfd) == -1)
+		return -1;
+
 	freeaddrinfo(servinfo);
 
         return 0;
 }
 
+int send_socket(int acceptedfd){
+	int BUFFER_SIZE2=16;
+	int fd;
+        char text[BUFFER_SIZE2];
+	ssize_t bytes_read;
+
+	fd = open("/var/tmp/aesdsocketdata", O_RDONLY);
+	if(fd==-1){
+		perror("open");
+		return -1;	
+	}
+
+	bytes_read=read(fd, text, BUFFER_SIZE2-1);
+	printf("bytes_read: %d\n",bytes_read);
+	while(bytes_read>0){
+		printf("sending: %s\n",text);
+		
+
+		if(send(acceptedfd,text,bytes_read,0) == -1){
+			perror("send");
+			return -1;
+		}
+		bytes_read=read(fd, text, BUFFER_SIZE2-1);
+		printf("bytes_read: %d\n",bytes_read);
+	}
+
+	close(fd);
+
+	return 0;
+}
+
+int read_socket(int acceptedfd){
+        int stop_received=0;
+        char text[BUFFER_SIZE];
+        int file = open("/var/tmp/aesdsocketdata", O_RDWR | O_CREAT | O_TRUNC, 0755);
+
+        while(stop_received==0){
+
+                //Receive the data from socket
+                int bytes_received=recv(acceptedfd, text, BUFFER_SIZE-1,0);
+
+                if(bytes_received == -1){
+                        perror("read");
+			close(file);
+                        return -1;
+                }
+
+                if(bytes_received==0){
+                        //text[0]='\n';
+                        stop_received=1;
+                }
+                else{
+			text[bytes_received]='\0';
+                        printf("value read: %s\n",text);
+			
+			//writing the data into file
+            		if (write(file, text, 1)==-1){
+              	         	close(file);
+                	        perror("write");
+                        	return -1;
+                	}
+		}
+
+        }//*/
+        close(file);
+
+	return 0;
+}
